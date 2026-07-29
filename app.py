688f0f4b74b98bb3d01fe84340474bc02dc96651
Majok-Bol <bolmajok001@gmail.com>
2026-07-29 12:51:41 +0300

    setup env

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,request,flash
+from dotenv import load_dotenv
+from flask_wtf import FlaskForm,CSRFProtect
+from wtforms import StringField,EmailField,PasswordField,SubmitField
+from wtforms.validators import Email,EqualTo
+from flask_sqlachemy import SQLAlchemy
+import os
+load_dotenv()
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=os.getenv("DATABASE_URL")
+db=SQLAlchemy(app)
+# print("DB: ",dbs)
 @app.route("/")
 def hello():
     return 'Hello world'
